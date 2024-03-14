e22875136eeda725b4dd23aa4c383b0a0c7d8e62
Izarloa99 <izarloa99@gmail.com>
2024-03-13 22:07:06 -0300

    Conteúdo da aula do dia 13-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/isabela/secao02/ArteAsciil.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.isabela.secao02;
+
+public class ArteAsciil {
+
+	public static void main(String[] args) {
+System.out.println("  ▓▓▓▓  ");
+System.out.println(" ▓    ▓");
+System.out.println("▓ ▓  ▓ ▓");
+System.out.println("▓      ▓");
+System.out.println("▓ ▓  ▓ ▓");
+System.out.println("▓  ▓▓  ▓");
+System.out.println(" ▓    ▓");
+System.out.println("  ▓▓▓▓");
+	}
+
+}
