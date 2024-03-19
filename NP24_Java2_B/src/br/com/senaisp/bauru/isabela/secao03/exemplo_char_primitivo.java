4e713893832c622814c60bebd6a88db0759cee5b
Izarloa99 <izarloa99@gmail.com>
2024-03-18 22:03:43 -0300

    Conteúdo da aula do dia 18-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/isabela/secao03/exemplo_char_primitivo.java
@@ -0,0 +1,15 @@
+package br.com.senaisp.bauru.isabela.secao03;
+
+public class exemplo_char_primitivo {
+	public static void main(String[] args) {
+		char letter1 = 72;//'H'; //72
+		char letter2 = 'e'; //101
+		char letter3 = 'l'; //108
+		char letter4 = 'l'; //108
+		char letter5 = 'o'; //111
+		System.out.println(""+letter1 + letter2 + letter3 + letter4 + letter5);
+		System.out.println(""+(++letter1));
+		System.out.println("Oi".length());
+		
+	}
+}
